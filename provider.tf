7d3baaadc36b791f8660c8bdc852f9968df5d600
Jaswinder Kumar <jaswinder_kumar@waters.com>
2021-06-15 11:05:27 +0530

    added initial files

---
+++ provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = "= 0.14.5"
+}
+
+terraform {
+  required_providers {
+    sonarcloud = {
+      source  = "terraform.jaswinderkumar.com/jaswinderkumar/sonarcloud"
+      version = ">= 0.0.2"
+    }
+  }
+}
+
+provider "sonarcloud" {
+  host   = var.host
+  scheme = var.scheme
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "host" {
+  description = "contains the sonarcloud hostname"
+  default     = "sonarcloud.io"
+}
+
+variable "scheme" {
+  description = "contains the sonarcloud scheme name"
+  default     = "https"
+}
